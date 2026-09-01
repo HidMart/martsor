0f4970c3c287d72dfe4609323ef7b6ce018b129d
؟MARTO <programmers.atlantis@gmail.com>
2026-09-02 02:51:12 +0330

    Update errors.py

--- martsor/errors.py
+++ martsor/errors.py
@@ -3,9 +3,19 @@ class MartsorError(Exception):
 
 
 class APIError(MartsorError):
-    """Raised when the API returns an error."""
+    """Raised when the Soroush Plus API returns an error."""
 
-    def __init__(self, message, status_code=None, response=None):
+    def __init__(
+        self,
+        message,
+        error_code=None,
+        status_code=None,
+        response=None,
+        retry_after=None,
+    ):
         super().__init__(message)
+
+        self.error_code = error_code
         self.status_code = status_code
         self.response = response
+        self.retry_after = retry_after
