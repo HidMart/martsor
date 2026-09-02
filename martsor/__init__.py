502b4aa4ef48353d103fac8d45f3891b470dea95
؟MARTO <programmers.atlantis@gmail.com>
2026-09-02 17:08:10 +0330

    Update __init__.py

--- martsor/__init__.py
+++ martsor/__init__.py
@@ -1,7 +1,14 @@
-"""martsor - Python library for building Soroush Plus bots."""
+"""
+martsor - Python library for building Soroush Plus bots.
+"""
 
 from .client import Bot, Client
-from .errors import APIError, MartsorError
+
+from .errors import (
+    APIError,
+    MartsorError,
+)
+
 from .keyboards import (
     Button,
     ForceReply,
@@ -9,24 +16,55 @@ from .keyboards import (
     ReplyKeyboard,
     ReplyKeyboardRemove,
 )
-from .types import CallbackQuery, Chat, Message, Update, User
 
-__version__ = "0.2.0"
+from .types import (
+    CallbackQuery,
+    Chat,
+    Message,
+    Update,
+    User,
+)
+
+
+__version__ = "0.3.0"
+
+
+# =========================================================
+# Self Client
+# =========================================================
+
+try:
+    from .self_client import SelfClient
+except ImportError:
+    SelfClient = None
+
 
 __all__ = [
+    # Bot
     "Bot",
     "Client",
+
+    # Self Client
+    "SelfClient",
+
+    # Keyboards
     "Button",
     "InlineKeyboard",
     "ReplyKeyboard",
     "ReplyKeyboardRemove",
     "ForceReply",
+
+    # Types
     "User",
     "Chat",
     "Message",
     "CallbackQuery",
     "Update",
+
+    # Errors
     "MartsorError",
     "APIError",
+
+    # Version
     "__version__",
 ]
